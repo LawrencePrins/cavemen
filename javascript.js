29662a75483f450cd4e3b9fab3e1a911091cc944
LawrencePrins <55742904+LawrencePrins@users.noreply.github.com>
2021-06-03 00:21:57 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,36 @@
+//For Firefox we have to handle it in JavaScript 
+var vids = $("video"); 
+$.each(vids, function(){
+       this.controls = false; 
+}); 
+//Loop though all Video tags and set Controls as false
+
+
+function loadDino() {
+    var videoEl = document.getElementsByTagName('video')[0];
+    var sourceEl = videoEl.getElementsByTagName('source')[0];
+    sourceEl.src = 'videos/scene_dino.mp4';
+    videoEl.load();
+   }
+
+   function loadMeteor() {
+    var videoEl = document.getElementsByTagName('video')[0];
+    var sourceEl = videoEl.getElementsByTagName('source')[0];
+    sourceEl.src = 'videos/scene_meteor.mp4';
+    videoEl.load();
+   }
+
+   function loadAlien() {
+    var videoEl = document.getElementsByTagName('video')[0];
+    var sourceEl = videoEl.getElementsByTagName('source')[0];
+    sourceEl.src = 'videos/scene_alien.mp4';
+    videoEl.load();
+   }
+  
+   //Zorgt ervoor dat de video terug gaat naar scene_rest
+   function loadRest() {
+    var videoEl = document.getElementsByTagName('video')[0];
+    var sourceEl = videoEl.getElementsByTagName('source')[0];
+    sourceEl.src = 'videos/scene_rest.mp4';
+    videoEl.load();
+  }
